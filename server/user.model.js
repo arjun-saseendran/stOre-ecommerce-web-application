ecda6d5104171cfb5dca19b6780da57b6fc0982b
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-01 21:36:56 +0530

    add userSchema

---
+++ server/user.model.js
@@ -0,0 +1,41 @@
+import { Schema, model } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      miniLength: 3,
+      maxLength: 30,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    role: {
+      type: String,
+      enum: ["user", "admin"],
+      default: "user",
+    },
+    mobile: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      miniLength: 4,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg",
+    },
+  },
+  { timestamps: true }
+);
+
+export const User = model("User", userSchema);
