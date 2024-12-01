4a5b9e4df5467819d436671d4e2bfc324d57adf4
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-01 21:23:19 +0530

    add user.model.js

--- server/index.js
+++ server/index.js
@@ -1,8 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
+import {connectDB} from "../server/config/db.js";
 
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
 
-app.listen(port, () => console.log(`Server runing on port ${port}`));
+app.listen(port, () => {
+  connectDB();
+  console.log(`Server runing on port ${port}`);
+});
