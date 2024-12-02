7e57df1340ffe168943677f76c21a01779675946
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-02 09:50:18 +0530

    add sellerSchema

---
+++ server/models/seller.model.js
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+
+const sellerSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      miniLength: 6,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg",
+    },
+    prodcuts: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+  },
+  { timestamps: true }
+);
+
+export const Seller = model("Seller", sellerSchema);
