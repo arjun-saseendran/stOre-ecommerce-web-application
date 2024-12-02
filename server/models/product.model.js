63afef738642e7f8f889acb36ed12f5e43c0a462
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-02 09:41:22 +0530

    add productSchema

---
+++ server/models/product.model.js
@@ -0,0 +1,36 @@
+import { Schema, model } from "mongoose";
+
+const productSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      maxLength: 30,
+    },
+    description: {
+      type: String,
+      required: true,
+      miniLength: 20,
+      maxLength: 300,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    image: {
+      type: String,
+      default: "",
+    },
+    seller: {
+      type: Schema.Types.ObjectId,
+      ref: "Seller",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Product = model("Product", productSchema);
