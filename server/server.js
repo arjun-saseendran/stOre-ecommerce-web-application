7a8128bb80ed3a38f8e0c0f8e99867b1c0e08177
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-05 07:43:22 +0530

    change index.js name to server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import {connectDB} from "../server/config/db.js";
+import {connectDB} from "./config/db.js";
 
 const app = express();
 dotenv.config();
