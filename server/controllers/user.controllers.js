472f20d08a219905ecd752772d779db99930f60e
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-05 07:02:45 +0530

    add passwordHandler.js

---
+++ server/controllers/user.controllers.js
@@ -0,0 +1,19 @@
+import { User } from "../models/user.model.js";
+
+const signup = async (req, res) => {
+  const { name, email, mobile, password } = req.body;
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+export {signup}
---
+++ server/routes/user.routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+router.post('/signup')
+router.post('/login')
+router.post('/logout')
+
+export default router
