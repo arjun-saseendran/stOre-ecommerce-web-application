c23ab1cd7e99b8ad9e61eab3fe1d95b5e830c106
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-13 10:22:01 +0530

    add reviewRoutes

--- server/routes/v1/reviewRoutes.js
+++ server/routes/v1/reviewRoutes.js
@@ -1,9 +1,23 @@
-import {Router} from 'express'
-import { addReview } from '../../controllers/reviewControllers.js'
-import {userAuth} from '../../middlewares/userAuth.js'
+import { Router } from "express";
+import {
+  addReview,
+  getProductReview,
+  getAverageRating,
+  deleteReview,
+} from "../../controllers/reviewControllers.js";
+import { userAuth } from "../../middlewares/userAuth.js";
 
 // Configure router
-export const reviewRouter = Router()
+export const reviewRouter = Router();
 
 // Add review
-reviewRouter.post('/add-review', userAuth, addReview)
+reviewRouter.post("/add-review", userAuth, addReview);
+
+// Display product review
+reviewRouter.get("/get-review", userAuth, getProductReview);
+
+// Get average review
+reviewRouter.get("/get-avg-rating", userAuth, getAverageRating);
+
+// Delete review
+reviewRouter.delete("/delete-reveiw", userAuth, deleteReview);
