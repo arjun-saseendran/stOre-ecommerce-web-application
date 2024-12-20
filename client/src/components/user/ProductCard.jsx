e7dc362ac22af9889324cb08ef666bfa0c0c8eec
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-20 05:50:10 +0530

    add axiosInstance.jsx

---
+++ client/src/components/user/ProductCard.jsx
@@ -0,0 +1,146 @@
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import { apiHandler } from "../utils/apiHandler";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
+
+function ProductCard({ product }) {
+  // Get api url
+  const apiUrl = import.meta.env.VITE_API_URL;
+
+  const {dark} = useSelector((state)=> state.dark)
+
+  const addToCart = async (productId) => {
+    // Api call
+    const [response, error] = await apiHandler(
+      `${apiUrl}/api/v1/cart/add-product`,
+      "POST",
+      { productId }
+    );
+    if (response) {
+      console.log(response);
+    } else {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Card className="crd-box">
+      <Card.Img
+        className="crd-image object-fit-contain"
+        variant="top"
+        src={product.image}
+      />
+      <Card.Body>
+        <Link
+          className="text-decoration-none text-black"
+          to={`/product/${product._id}`}
+        >
+          <Card.Title className="crd-title title-hover">
+            {product.title}
+          </Card.Title>
+        </Link>
+        <Card.Text className="crd-description">{product.description}</Card.Text>
+        <Card.Text className=" crd-price fw-bold text-center fw-bolder h5">
+          ₹{product.price}
+        </Card.Text>
+        <div className="d-flex justify-content-center">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-black mb-3"
+            height="40px"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-black mb-3"
+            height="40px"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-black mb-3"
+            height="40px"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-secondary mb-3"
+            height="40px"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+              clipRule="evenodd"
+            />
+          </svg>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            className="size-6 text-secondary mb-3"
+            height="40px"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+              clipRule="evenodd"
+            />
+          </svg>
+
+          <span className="review-bar bg-black text-white fw-bold mt-2 ms-2">
+            5
+          </span>
+        </div>
+
+        <Button
+          className="w-100"
+          variant= {dark? 'dark' : 'primary'}
+          onClick={() => addToCart(product._id)}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth="1.5"
+            stroke="currentColor"
+            className="size-6 me-1"
+            height="25px"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
+            />
+          </svg>
+          Add to Cart
+        </Button>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default ProductCard;
---
+++ client/src/config/axiosInstance.jsx
@@ -0,0 +1,8 @@
+import axios from 'axios'
+
+// Create axios instance
+export const axiosInstance = axios.create({
+    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+    withCredentials: true
+
+})
