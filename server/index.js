8ce1eef8eabb5b5669ea9c26da5bb92d7e272028
arjun-saseendran <arjunsaseendran@icloud.com>
2024-12-29 10:07:48 +0530

    bug fix

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import cors from "cors";
+import cors from 'cors'
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db.js";
 import { apiRouter } from "./routes/index.js";
@@ -14,15 +14,12 @@ const app = express();
 // Config cors
 app.use(
   cors({
-    origin: [
-      "https://store-ecommerce-web-application-client.vercel.app",
-      "http://localhost:5173",
-    ],
+    origin: process.env.CORS,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 
 // Configure port
 const PORT = process.env.PORT;
