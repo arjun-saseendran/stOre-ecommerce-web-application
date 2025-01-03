4a4eb01245b3e1143d5c8f08aa1306319f1a01ec
arjun-saseendran <arjunsaseendran@icloud.com>
2025-01-03 14:31:54 +0530

    add payment success and cancelled

--- client/src/pages/user/PaymentCancel.jsx
+++ client/src/pages/user/PaymentCancel.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import { useEffect } from "react";
+import { Container, Button } from "react-bootstrap";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import { axiosInstance } from "../../config/axiosInstance";
 
 export const PaymentCancel = () => {
+  // Get current theme
+  const { theme } = useSelector((state) => state.theme);
+
+  useEffect(() => {
+    // Handle stock and clear cart
+    const updateStockAndClearCart = async () => {
+      try {
+        // Update stock
+        const stockUpdate = await axiosInstance({
+          method: "POST",
+          url: "/order/update-stock",
+        });
+        console.log(stockUpdate);
+
+        // Clear cart
+        const clearCart = await axiosInstance({
+          method: "DELETE",
+          url: "/cart/clear-cart",
+        });
+        console.log(clearCart);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    updateStockAndClearCart();
+  }, []);
+
   return (
-    <div>PaymentCancel</div>
-  )
-}
+    <Container style={{minHeight: '400px'}}>
+      <div className="d-flex justify-content-center align-items-center mx-auto vh-100">
+        <div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="size-6"
+            height={'400px'}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.182 16.318A4.486 4.486 0 0 0 12.016 15a4.486 4.486 0 0 0-3.198 1.318M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
+            />
+          </svg>
+        </div>
+        <div>
+          <h1 className="text-white text-center mt-5">Payment Cancelled!</h1>
+          <Button variant={theme ? "warning" : "dark"} className="text-white">
+            <Link className="text-decoration-none text-white" to={"/"}>
+              Back to home
+            </Link>
+          </Button>
+        </div>
+      </div>
+    </Container>
+  );
+};
--- client/src/pages/user/PaymentSuccess.jsx
+++ client/src/pages/user/PaymentSuccess.jsx
@@ -34,7 +34,7 @@ export const PaymentSuccess = () => {
   }, []);
 
   return (
-    <Container>
+    <Container style={{minHeight: '400px'}}>
       <div className="d-flex justify-content-center align-items-center mx-auto vh-100">
         <div>
           <svg
@@ -43,13 +43,13 @@ export const PaymentSuccess = () => {
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-6 text-white"
-            height="400px"
+            className="size-6"
+            height={'400px'}
           >
             <path
               strokeLinecap="round"
               strokeLinejoin="round"
-              d="M15 8.25H9m6 3H9m3 6-3-3h1.5a3 3 0 1 0 0-6M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+              d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z"
             />
           </svg>
         </div>
