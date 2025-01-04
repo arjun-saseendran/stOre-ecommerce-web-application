4c7515d698615c241297ddb1b52852db6de4a548
arjun-saseendran <arjunsaseendran@icloud.com>
2025-01-04 11:55:05 +0530

    add error loading

--- client/src/pages/user/Home.jsx
+++ client/src/pages/user/Home.jsx
@@ -1,11 +1,13 @@
 import { ProductList } from "./ProductList";
 import { Carrousel } from "../../components/user/Carrousel";
+import {Error} from '../../components/shared/Error'
 
 export const Home = () => {
   return (
     <div>
-      <Carrousel />
-      <ProductList />
+      {/* <Carrousel />
+      <ProductList /> */}
+      <Error/>
     </div>
   );
 };
