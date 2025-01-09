d18ba16cc49f0708fa8cc7de274074e0ea5c20bb
arjun-saseendran <arjunsaseendran@icloud.com>
2025-01-09 21:06:40 +0530

    add remove strict mode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,12 @@
-import { StrictMode } from "react";
 import { Provider } from "react-redux";
-import {store} from './redux/store'
+import { store } from "./redux/store";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
