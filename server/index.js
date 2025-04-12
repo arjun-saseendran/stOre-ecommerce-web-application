234907590cb6c25ab37f42fa2b7fd076e5fe01a0
arjun-saseendran <arjunsaseendran@icloud.com>
2025-04-12 20:28:34 +0530

    update cors

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ const app = express();
 // Config cors
 app.use(
   cors({
-    origin: process.env.CORS,
+    origin: [process.env.CORS,'http://localhost:5173'],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
