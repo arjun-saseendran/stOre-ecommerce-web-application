b810b772701b6fb9826d6c89242aa1bb3f700ccb
arjun-saseendran <arjunsaseendran@icloud.com>
2025-04-29 21:54:33 +0530

    update cors

--- server/middlewares/userAuth.js
+++ server/middlewares/userAuth.js
@@ -14,6 +14,7 @@ export const userAuth = async (req, res, next) => {
 
     // Decoding token
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
     // Handle no decoded
     if (!decoded) {
       return res.status(401).json({ message: "User not autherized" });
